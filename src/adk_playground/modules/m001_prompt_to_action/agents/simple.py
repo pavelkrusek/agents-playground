2c04abe20630461169f2df56e6688004c935d256
Pavel Krusek <pavel@krusek.dk>
2025-11-26 22:13:16 +0100

    feat: better simple agent

--- src/adk_playground/modules/m001_prompt_to_action/agents/simple.py
+++ src/adk_playground/modules/m001_prompt_to_action/agents/simple.py
@@ -1,5 +1,6 @@
 from google.adk.agents import Agent
 from google.adk.runners import Runner
+from google.adk.tools import google_search
 
 from adk_playground.core.settings import MODEL
 
@@ -8,10 +9,13 @@ def build_agent() -> Agent:
     return Agent(
         name="action_agent",
         model=MODEL,
+        description="A simple agent that can answer general questions.",
         instruction=(
             "You are a helpful assistant. Understand the user's request and produce a direct, high-quality answer."
             "If the request is open-ended, produce the content directly."
+            "Use Google Search for current info or if unsure."
         ),
+        tools=[google_search],
         output_key="final_output",
     )
 
